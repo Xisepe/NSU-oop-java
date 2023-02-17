7b7d28ae38c4cef3692e84e6cc386e037771832e
Xisepe <maxim.golubeff2014@gmail.com>
2023-02-17 21:19:21 +0700

    ArgCalculatorContextCommand added field args

--- Task2/src/main/java/models/command/impl/calculator/ArgCalculatorContextCommand.java
+++ Task2/src/main/java/models/command/impl/calculator/ArgCalculatorContextCommand.java
@@ -2,6 +2,9 @@ package models.command.impl.calculator;
 
 import lombok.experimental.SuperBuilder;
 
+import java.util.List;
+
 @SuperBuilder
 public abstract class ArgCalculatorContextCommand extends CalculatorContextCommand{
+    private final List<Object> args;
 }
